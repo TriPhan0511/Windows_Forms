0bf7985cb8070c846d6b31bbbe17a47b0c098b3d
Tri Phan <phanphutri@gmail.com>
2022-05-03 11:28:56 +0700

    Updated

---
+++ Windows_Forms_Tutorials/MathQuiz/Form1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MathQuiz
+{
+    public partial class Form1 : Form
+    {
+        // Create a Random object called randomizer
+        // to generate random numbers
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Windows_Forms_Tutorials/Sample_Console_App/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sample_Console_App
+{
+    internal class Program
+    {
+        // A sample about Radom class
+        // The following example creates a single random generator and call its NextBytes, Next, and NextDouble
+        // methods to generate sequences of random numbers within different ranges.
+        static void Main(string[] args)
+        {
+            // Instantiate random number generator using system-supplied value as seed
+            var rand = new Random();
+
+            //// Generate and display 5 random byte (integer) values
+            //var bytes = new byte[5];
+            //rand.NextBytes(bytes);
+            //Console.WriteLine("Five random byte values:");
+            //foreach (var item in bytes)
+            //{
+            //    Console.Write("{0, 5}", item);
+            //}
+            //Console.WriteLine();
+            //// -> OUTPUT:
+            ////Five random byte values:
+            ////56  164  238  124   76
+
+            //// Generate and display 5 random integers
+            //Console.WriteLine("Five random integer values:");
+            //for (int i = 0; i < 5; i++)
+            //{
+            //    //Console.Write($"{rand.Next()} ");
+            //    Console.Write("{0,15:N0}", rand.Next());
+            //}
+            //Console.WriteLine();
+            //// -> OUTPUT:
+            ////Five random integer values:
+            ////1,419,969,413    534,276,553  1,304,236,915  1,515,200,137    444,375,257
+            ///
+
+            //// Create and display 5 random integers between 0 and 100
+            //Console.WriteLine("Five random integers between 0 and 100:");
+            //for (int i = 0; i < 5; i++)
+            //{
+            //    Console.Write($"{rand.Next(101)} ");
+            //}
+            //Console.WriteLine();
+            //// -> OUTPUT:
+            ////Five random integers between 0 and 100:
+            ////54 13 84 73 5
+
+            // Create and display five random integers from 50 to 100
+            Console.WriteLine("Five random integers from 50 to 100:");
+            for (int i = 0; i < 5; i++)
+            {
+                Console.Write($"{rand.Next(50, 101)} ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
